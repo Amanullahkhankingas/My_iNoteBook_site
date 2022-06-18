2b9c00dc1e3466df79f8d979484aa348e5024e48
Aman Ullah <84645417+Amanullahkhankingas@users.noreply.github.com>
2022-06-19 01:27:34 +0500

    lecture 41 'intorduction to MERN stack ' theory , and lecture 42 'Project 3- iNotebook backend and React frontend setup' ,and lecture 43 'iNotebook db and Express Server setup'

---
+++ backend/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const mongoURI ="mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false "
+
+
+
+const connectToMongo =()=>{
+    mongoose.connect(mongoURI,()=>{
+        console.log('connected to mongo successfully db.js module')
+    })
+}
+
+
+module.exports = connectToMongo;
